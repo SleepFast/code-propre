7178ed4fa816454d09d55b2e74c009fc8ad1a0b8
SleepFast <cartmam1@hotmail.fr>
2023-07-10 16:40:20 +0200

    exo 4

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -4,16 +4,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtils {
+	private static final SimpleDateFormat formatter = new SimpleDateFormat();
 
-	public static String format(String pattern, Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
-		return formateur.format(date);
+	public static String format(final Date date) {
+		formatter.applyPattern("dd/MM/yyyy HH:mm:ss");
+
+		return formatter.format(date);
 	}
-	
-	public static String formatDefaut(Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		return formateur.format(date);
+
+	public static String format(final String pattern, final Date date) {
+		formatter.applyPattern(pattern);
+
+		return formatter.format(date);
 	}
 }
