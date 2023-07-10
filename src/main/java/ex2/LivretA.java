564ac5b7d467faf050fe7e864b43cd88062fbae7
SleepFast <cartmam1@hotmail.fr>
2023-07-10 15:32:22 +0200

    ex 2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -2,8 +2,43 @@ package ex2;
 
 public class LivretA extends CompteBancaire {
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	private double tauxRemuneration;
+
+	public LivretA(double solde, double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
+		super(solde, 0);
 	}
 
+	/** 
+	 * @Override
+	 *  
+	 * Débite un montant au solde
+	 * 
+	 * @param montant
+	 */
+	public void debiterMontant(double montant){
+
+		if (this.solde - montant > 0){
+			this.solde = solde - montant;
+		} 
+	}
+
+	public void appliquerRemuAnnuelle(){
+
+		this.solde = solde + solde*tauxRemuneration/100;
+	}
+
+	/** Getter for tauxRemuneration
+	 * @return the tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+	
+	/** Setter
+	 * @param tauxRemuneration the tauxRemuneration to set
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
+	}
 }
